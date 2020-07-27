8e5ab4ec11c1db47ab4da8c01b94d843bff271c0
Jesse Quijano <jesse@quijano.net>
2020-07-27 10:11:02 -0700

    final cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -21,20 +21,37 @@ class App extends React.Component {
 			}
 		});
 		
-		this.setState({ videos: response.data.items });
+		this.setState({ 
+			videos: response.data.items,
+			selectedVideo: response.data.items[0]
+		});
 	};
 
 	onVideoSelect = (video) => {
 		console.log('From the app!', video);
-		this.setState({selectedVideo : video});
+		this.setState({
+			selectedVideo : video
+		});
+	}
+
+	componentDidMount() {
+		this.onTermSubmit('buildings');
 	}
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+				<div className="ui grid">
+					<div className="ui row">
+						<div className="eleven wide column">
+							<VideoDetail video={this.state.selectedVideo} />
+						</div>
+						<div className="five wide column">
+							<VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
